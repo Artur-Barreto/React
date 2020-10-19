32685810c15317444092626065525ea46fdf9d34
ArtursFreitas <arturfreitasdev@outlook.com>
2020-10-19 14:39:38 -0300

    criando tabela com react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  makeStyles,
+  TableBody,
+  Table,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+} from "@material-ui/core";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const useStyle = makeStyles({
+  table: {
+    minWidth: 650,
+  },
+});
+
+function createData(name, calorias, gordura, carboidratos, proteinas) {
+  return { name, calorias, gordura, carboidratos, proteinas };
 }
 
-export default App;
+const rows = [
+  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+  createData("Eclair", 262, 16.0, 24, 6.0),
+  createData("Cupcake", 305, 3.7, 67, 4.3),
+  createData("Gingerbread", 356, 16.0, 49, 3.9),
+];
+
+export default function BasicTable() {
+  const classes = useStyle();
+
+  return (
+    <TableContainer component={Paper}>
+      <Table className={classes.table} aria-label="simple-table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Dessert (100g serving)</TableCell>
+            <TableCell align="right">Calorias</TableCell>
+            <TableCell align="right">gordura&nbsp;(g)</TableCell>
+            <TableCell align="right">carboidratos&nbsp;(g)</TableCell>
+            <TableCell align="right">proteinas&nbsp;(g)</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow key={row.name}>
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="right">{row.calorias}</TableCell>
+              <TableCell align="right">{row.gordura}</TableCell>
+              <TableCell align="right">{row.carboidratos}</TableCell>
+              <TableCell align="right">{row.proteinas}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
